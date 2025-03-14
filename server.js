1775ee435dba1ad61d5a79ff7abe3fa6b2b11090
goktugokce <goktugokce@gmail.com>
2025-03-15 00:00:36 +0100

    amplify2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env.development' });
+require("dotenv").config({ path: '.env.development' });
 const express = require("express");
 const chalk =  require('chalk');
 const bodyParser = require("body-parser");
